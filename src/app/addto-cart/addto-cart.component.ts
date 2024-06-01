63428c4509ad70f427947e2730898952e9e2c45e
Ribhav Falwaria <ribhavfalwaria48@gmail.com>
2024-06-01 18:31:35 +0530

    updated tags

--- src/app/addto-cart/addto-cart.component.ts
+++ src/app/addto-cart/addto-cart.component.ts
@@ -13,32 +13,32 @@ export class AddtoCartComponent {
   constructor(private $gaService: GoogleAnalyticsService){}
 
   submit(){
-  //   this.$gaService.gtag("event", "add_to_cart", {
-  //     currency: "USD",
-  //     value: 30.03,
-  //     items: [
-  //       {
-  //         item_id: "SKU_12345",
-  //         item_name: "Stan and Friends Tee",
-  //         affiliation: "Google Merchandise Store",
-  //         coupon: "SUMMER_FUN",
-  //         discount: 2.22,
-  //         index: 0,
-  //         item_brand: "Google",
-  //         item_category: "Apparel",
-  //         item_category2: "Adult",
-  //         item_category3: "Shirts",
-  //         item_category4: "Crew",
-  //         item_category5: "Short sleeve",
-  //         item_list_id: "related_products",
-  //         item_list_name: "Related Products",
-  //         item_variant: "green",
-  //         location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-  //         price: 10.01,
-  //         quantity: 3
-  //       }
-  //     ]
-  //   })
+    this.$gaService.gtag("event", "add_to_cart", {
+      currency: "USD",
+      value: 30.03,
+      items: [
+        {
+          item_id: "SKU_12345",
+          item_name: "Stan and Friends Tee",
+          affiliation: "Google Merchandise Store",
+          coupon: "SUMMER_FUN",
+          discount: 2.22,
+          index: 0,
+          item_brand: "Google",
+          item_category: "Apparel",
+          item_category2: "Adult",
+          item_category3: "Shirts",
+          item_category4: "Crew",
+          item_category5: "Short sleeve",
+          item_list_id: "related_products",
+          item_list_name: "Related Products",
+          item_variant: "green",
+          location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+          price: 10.01,
+          quantity: 3
+        }
+      ]
+    })
 
   this.$gaService.gtag("event", "purchase", {
     // This purchase event uses a different transaction ID
@@ -59,7 +59,7 @@ export class AddtoCartComponent {
       coupon: "SUMMER_FUN",
       discount: 2.22,
       index: 0,
-      item_brand: "Google",
+      item_brand: "Apple",
       item_category: "Apparel",
       item_category2: "Adult",
       item_category3: "Shirts",
