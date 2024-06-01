136e733e2a741d73ac201534f3b18e1dce32f1c7
Ribhav Falwaria <ribhavfalwaria48@gmail.com>
2024-06-01 19:16:02 +0530

    separated events

--- src/app/addto-cart/addto-cart.component.ts
+++ src/app/addto-cart/addto-cart.component.ts
@@ -43,42 +43,7 @@ export class AddtoCartComponent {
 
 }
 
-purchase(){
 
-  this.$gaService.gtag("event", "purchase", {
-    // This purchase event uses a different transaction ID
-    // from the previous purchase event so Analytics
-    // doesn't deduplicate the events.
-    // Learn more: https://support.google.com/analytics/answer/12313109
-    transaction_id: "T_12345_2",
-    value: 30.03,
-    tax: 4.90,
-    shipping: 5.99,
-    currency: "USD",
-    coupon: "SUMMER_SALE",
-    items: [
-    {
-      item_id: "SKU_12345",
-      item_name: "Stan and Friends Tee",
-      affiliation: "Google Merchandise Store",
-      coupon: "SUMMER_FUN",
-      discount: 2.22,
-      index: 0,
-      item_brand: "Apple",
-      item_category: "Apparel",
-      item_category2: "Adult",
-      item_category3: "Shirts",
-      item_category4: "Crew",
-      item_category5: "Short sleeve",
-      item_list_id: "related_products",
-      item_list_name: "Related Products",
-      item_variant: "green",
-      location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-      price: 10.01,
-      quantity: 3
-    }]
-})
-}
   
 
 }
